8dccc08ee964dc9426cf2ecc785d125b6eb8537f
Stephen-Schneider <s.f.schneider>
2026-01-21 13:58:26 -0600

    LeapYear

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,13 @@
+package studio1;
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args){
+        boolean isLeapYear;
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is the year: ");
+        int year = in.nextInt();
+        isLeapYear = (year%4==0 && year%100!=0) || year%400==0;
+        System.out.println(year + " is a leap year: " + isLeapYear);
+    }
+}
